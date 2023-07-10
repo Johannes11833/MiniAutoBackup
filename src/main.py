fb00c968626648d006b6e6cc2a6c108e96b72330
Johannes11833 <j.gundlach@me.com>
2023-07-10 21:18:40 +0200

    Add env variable to start backup right after start

--- src/main.py
+++ src/main.py
@@ -29,6 +29,7 @@ if __name__ == "__main__":
 
     # load ENV-variables
     load_dotenv()
+    BACKUP_ON_START = os.getenv("BACKUP_ON_START") or False
     REMOTE_EXPORT_PATH = os.getenv("REMOTE_EXPORT_PATH")
     if REMOTE_EXPORT_PATH is None:
         log.error("The env variable REMOTE_EXPORT_PATH is not set.")
@@ -41,6 +42,12 @@ if __name__ == "__main__":
     for schedule in UPLOAD_TIMES:
         ts.add(DailyTask(schedule, upload))
         log.info(f"Scheduling daily uploads @ {schedule}")
+
+    if BACKUP_ON_START:
+        log.info('Starting backup right away as "BACKUP_ON_START" is set.')
+        upload()
+
+    # start the scheduler
     ts.start()
 
     # wait until manually stopped
